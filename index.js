33c13e9095dae9d38dd08b14de2e670d4ae59f2e
Abidur Rahman Akash <abidurrahman471@gmail.com>
2021-03-23 21:06:38 +0600

    file updated

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Get & Post to Homepage");
+  res.send("Get request to homepage");
+});
+
+app.get("/fruits/banana", (req, res) => {
+  const fruitDetails = {
+    fruit: "banana",
+    quantity: 1000,
+    price: 10000,
+  };
+  res.send(fruitDetails);
+});
+
+const userName = ["Akash", "Rakib", "Sakib", "Parvej", "Pankaj"];
+
+app.get("/users/:id", (req, res) => {
+  const value = req.params.id;
+  const name = userName[value];
+  res.send({ value, name });
+});
+
+// post
+
+app.post("/addUser", (req, res) => {
+  const user = req.body;
+  user.id = 01;
+
+  res.send(user);
 });
 
 app.listen(3000, () => {
